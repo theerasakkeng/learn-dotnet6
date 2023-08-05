35d8caa19da87c896b5f0fb3b09ebd7aee01c384
theerasakkeng <theerasak_fusrodah@hotmail.com>
2023-08-06 06:44:25 +0700

    updastedb

--- TestApp/Models/Entities.cs
+++ TestApp/Models/Entities.cs
@@ -24,15 +24,6 @@ namespace TestApp.Models
         public virtual DbSet<location> locations { get; set; } = null!;
         public virtual DbSet<region> regions { get; set; } = null!;
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ExampleDB;User id=sa;Password=Keng1234");
-//            }
-//        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<country>(entity =>
